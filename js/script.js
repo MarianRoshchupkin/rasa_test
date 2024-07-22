d87c9b8af1532d90e50361580281a6b7ee8015f4
Марьян Рощупкин <marianroshchupkin@MacBook-Pro--Marian.local>
2024-07-23 03:21:07 +1000

    completed layout for tablet

--- js/script.js
+++ js/script.js
@@ -18,3 +18,60 @@ window.handleDropdownClick = (menuClassName, iconClassName, menuActiveClassName,
   menu.classList.toggle(menuActiveClassName);
   icon.classList.toggle(iconActiveClassName);
 }
+
+window.handleShowMoreClick = (
+  mainContainerClassName, 
+  buttonClassName, 
+  containerClassName, 
+  elementsClassName, 
+  elementsClassNameInactive
+) => {
+  const mainContainer = document.querySelector(mainContainerClassName);
+  const container = document.querySelector(containerClassName);
+  const button = document.querySelector(buttonClassName);
+  const list = document.querySelectorAll(elementsClassName);
+  const listArray = [...list];
+
+  listArray.forEach((item) => 
+    item.classList.remove(elementsClassNameInactive)
+  );
+
+  mainContainer.removeChild(button);
+  container.innerHTML = "";
+  container.append(...listArray);
+}
+
+window.handleScreenResizeOnload = (elementsClassName, elementsClassNameInactive) => {
+  const screenWidth = window.innerWidth;
+  const list = document.querySelectorAll(elementsClassName);
+  const totalElements = list.length;
+  const numVisible = getNumVisibleElements(screenWidth, totalElements);
+  
+  updateElementVisibility(elementsClassName, elementsClassNameInactive, numVisible);
+};
+
+function getNumVisibleElements(screenWidth, totalElements) {
+  const MOBILE_SCREEN_AMOUNT = 3;
+  const TABLET_SCREEN_AMOUNT = 6;
+  
+  if (screenWidth < 768) {
+    return MOBILE_SCREEN_AMOUNT;
+  } else if (screenWidth < 1440) {
+    return TABLET_SCREEN_AMOUNT;
+  } else {
+    return totalElements;
+  }
+}
+
+function updateElementVisibility(elementsClassName, elementsClassNameInactive, numVisible) {
+  const list = document.querySelectorAll(elementsClassName);
+  const listArray = [...list];
+
+  listArray.forEach((item, index) => {
+    if (index < numVisible) {
+      item.classList.remove(elementsClassNameInactive);
+    } else {
+      item.classList.add(elementsClassNameInactive);
+    }
+  });
+};
